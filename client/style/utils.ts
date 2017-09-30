fdb252aca0a19576047fd86b8ed5fe53b75a0406
JPSchellenberg <jps@Lumi.education>
2017-10-01 00:15:39 +0200

    add style utils

---
+++ client/style/utils.ts
@@ -0,0 +1,13 @@
+export function state_color(state: string): string {
+	switch (state) {
+		case 'init':
+		default:
+			return '#2c80b9';
+		case 'pending':
+			return '#f39c12';
+		case 'error':
+			return '#c0392c';
+		case 'success':
+			return '#27ae60';
+	}
+}
