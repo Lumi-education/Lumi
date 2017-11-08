eb2dcd526023903f5924ae869e4b3f0a3fc23ad9
JPSchellenberg <jps@Lumi.education>
2017-11-09 00:13:21 +0100

    update multiplichoice-card

--- client/components/cards/multiplechoice.tsx
+++ client/components/cards/multiplechoice.tsx
@@ -5,13 +5,13 @@ import * as markdownit 	from 'markdown-it';
 const md = markdownit();
 
 import Paper 			from 'material-ui/Paper';
-
 type Markdown = string;
 
 interface IProps {
 	text: Markdown;
 	items: Array<Markdown>;
 	show_correct_values?: boolean;
+	selected_items?: Array<Markdown>;
 }
 
 interface IState {}
@@ -30,7 +30,16 @@ export default class MultiplechoiceCard extends React.Component<IProps, IState> 
 
 				{
 					this.props.items.map((item) =>
-						<Paper style={{ padding: '2px', margin: '10px', backgroundColor: backgroundColor(true, item) }}>
+						<Paper 
+							style={{ 
+								padding: '2px', 
+								margin: '10px', 
+								backgroundColor: backgroundColor(
+										this.props.show_correct_values || false, 
+										item,
+										this.props.selected_items || []
+								) }}
+						>
 							<div dangerouslySetInnerHTML={{ __html: md.render(item.replace(/^x |^o /, '')) || '# No Markdown' }} />
 						</Paper>
 					)
@@ -42,12 +51,12 @@ export default class MultiplechoiceCard extends React.Component<IProps, IState> 
 	}
 }
 
-function backgroundColor(show_correct_values: boolean, item: string): string {
+function backgroundColor(show_correct_values: boolean, item: string, selected_items: Array<string>): string {
 	if (show_correct_values) {
-		if (item.charAt(0) === 'x') { return 'green'; } 
-		if (item.charAt(0) === 'o') { return 'red'; }
-		return 'blue';
+		if (item.charAt(0) === 'x') { return '#1abc9c'; } 
+		if (item.charAt(0) === 'o') { return '#c0392b'; }
+		return '#3498db';
 	} else {
-		return '#FF00FF';
+		if (selected_items.indexOf(item) > -1) { return '#3498db'; } else { return '#FFFFFF'; }
 	}
 }
