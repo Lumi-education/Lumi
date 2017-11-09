365f7767b8f31c3f2041fb7b53867a1c334ce65c
JPSchellenberg <jps@Lumi.education>
2017-11-09 16:04:10 +0100

    update multiplechoice-card-component

--- client/components/cards/multiplechoice.tsx
+++ client/components/cards/multiplechoice.tsx
@@ -1,5 +1,10 @@
 import * as React 		from 'react';
 import { connect } 		from 'react-redux';
+import { 
+	isEqual,
+	pull,
+	noop
+} 						from 'lodash';
 
 import * as markdownit 	from 'markdown-it';
 const md = markdownit();
@@ -12,6 +17,7 @@ interface IProps {
 	items: Array<Markdown>;
 	show_correct_values?: boolean;
 	selected_items?: Array<Markdown>;
+	cb?: (selected_items: Array<Markdown>, score: number) => void;
 }
 
 interface IState {}
@@ -19,12 +25,28 @@ interface IState {}
 export default class MultiplechoiceCard extends React.Component<IProps, IState> {
 	constructor(props: IProps) {
 		super(props);
+
+		this.handleClick = this.handleClick.bind( this );
+	}
+	
+	public handleClick(selected_item: string) {
+		const newArray = [ ...this.props.selected_items ];
+		if (this.props.selected_items.some((item) => item === selected_item)) {
+			pull(newArray, selected_item);
+		} else {
+			newArray.push(selected_item);
+		}
+
+		const correct_answers = this.props.items.filter((i) => i.charAt(0) === 'x' || i.charAt(0) === 'X');
+
+		const score = isEqual(newArray.slice(0).sort() , correct_answers.sort()) ? 1 : 0;
+		this.props.cb ? this.props.cb(newArray, score) : noop();
 	}
 
 	public render() {
 			return (
 			<div>
-				<Paper style={{ padding: '20px', margin: '10px' }} >
+				<Paper style={{ padding: '10px' }} >
 					<div dangerouslySetInnerHTML={{ __html: md.render(this.props.text || '# No markdown') }} />
 				</Paper>
 
@@ -39,6 +61,7 @@ export default class MultiplechoiceCard extends React.Component<IProps, IState> 
 										item,
 										this.props.selected_items || []
 								) }}
+							onClick={() => this.handleClick(item)}
 						>
 							<div dangerouslySetInnerHTML={{ __html: md.render(item.replace(/^x |^o /, '')) || '# No Markdown' }} />
 						</Paper>
