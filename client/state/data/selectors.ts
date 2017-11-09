2a62777dae13013fa33988359e57a8e57ff63561
JPSchellenberg <jps@Lumi.education>
2017-11-09 17:29:06 +0100

    update data-selector

--- client/state/data/selectors.ts
+++ client/state/data/selectors.ts
@@ -7,6 +7,19 @@ import {
 
 export function select_data(state: IState, collection_id: string, card_id: string) {
 	return state.data.map.toArray()
-	.filter(data => (data as any).collection_id === collection_id && (data as any).card_id === card_id)[0]
+	.filter(data => 
+		(data as any).collection_id === collection_id && 
+		(data as any).card_id === card_id && 
+		(data as any).data_type === 'card')[0]
+	|| {};
+}
+
+export function select_data_as_map(state: IState) {
+	return state.data.map;
+}
+
+export function select_data_for_collection(state: IState, collection_id: string) {
+	return state.data.map.toArray()
+	.filter(data => (data as any).collection_id === collection_id && (data as any).data_type === 'collection')[0]
 	|| {};
 }
