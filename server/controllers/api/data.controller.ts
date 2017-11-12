e066218250d158d98d51b3c7d8f0acb8d2009f45
JPSchellenberg <jps@Lumi.education>
2017-11-12 16:43:02 +0100

    server: add data-api

---
+++ server/controllers/api/data.controller.ts
@@ -0,0 +1,35 @@
+import * as express 		from 'express';
+import { assign } 			from 'lodash';
+import { Request } 			from '../../middleware/auth';
+
+import Data 				from '../../models/Data';
+import { DB } 				from '../../db';
+
+import Controller 			from '../controller';
+
+class DataController extends Controller<Data> {
+	public create(req: Request, res: express.Response) {
+		
+			const db = new DB(res);
+		
+			db.insert( new Data( req.body ) );
+			
+	}
+	
+	public find(req: Request, res: express.Response) {
+		
+			const db = new DB(res);
+		
+			db.find(
+				assign({}, { type: 'data' }, req.query),
+				{},
+				(data: Array<Data>) => {
+					res.status(200).json({ data: data });
+				},
+				Data
+			);
+			
+	}
+}
+
+export default new DataController('data');
