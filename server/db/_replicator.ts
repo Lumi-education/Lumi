d29d6afaa9de38577f02cab7ee714ef1a523ba88
JPSchellenberg <jps@Lumi.education>
2017-11-14 22:51:19 +0100

    remove db-replication-setup

--- server/db/_replicator.ts
+++
@@ -1,7 +0,0 @@
-export const init = {
-	'_id': 'init',
-	'source':  'http://devdb.lumi.education/init',
-	'target':  ( (process.env.DB_HOST || 'http://localhost:5984') + '/' + (process.env.DB || 'lumidb') ),
-	'continuous':  true,
-	'create_target':  true
-};
