0bc037b76c934d84573bc8a708a02c7ea3dd82a3
Frederik Vanhevel <fv@hubrick.com>
2017-11-15 11:15:22 +0100

    fix: client linting errors and add server linting

---
+++ tsconfig.server.js
@@ -0,0 +1,14 @@
+{
+    "compilerOptions": {
+        "module": "commonjs",
+        "moduleResolution": "node",
+        "jsx": "react",
+        "emitDecoratorMetadata": true,
+        "experimentalDecorators": true,
+        "removeComments": false,
+        "noImplicitAny": false,
+        "allowJs": true,
+        "outDir": "build/"
+    },
+    "include": ["node_modules/@types/**/*.d.ts", "server/boot.ts"]
+}
