f4c4b6e92cb6baf64d0376151a0145ebfbbdf104
Frederik Vanhevel <fv@hubrick.com>
2017-11-20 21:11:51 +0100

    fix: re-add tap event plugin

--- app/client/boot.tsx
+++ app/client/boot.tsx
@@ -1,18 +1,22 @@
+declare var process;
+
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-
 import { browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
+import * as injectTapEventPlugin from 'react-tap-event-plugin';
 
 import Router from './router';
 import store from 'client/store';
 
-declare var process;
-
 import theme from './style/theme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 if (localStorage.getItem('lumi_version') !== process.env.VERSION) {
     localStorage.clear();
