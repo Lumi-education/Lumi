b91594106d904d2eeef81148fdbaa0ef49c99cfa
JPSchellenberg <jps@Lumi.education>
2017-11-20 08:47:37 +0100

    add user-data-api

--- app/server/controllers/api/user.controller.ts
+++ app/server/controllers/api/user.controller.ts
@@ -1,13 +1,28 @@
 import * as express from 'express';
-
+import { assign } from 'lodash';
 import { DB } from '../../db';
 import { IRequest } from '../../middleware/auth';
 
 import User from '../../models/User';
 import Group from '../../models/Group';
 import Collection from '../../models/Collection';
+import Data from '../../models/Data';
 
 export class UserController {
+    public createData(req: IRequest, res: express.Response) {
+        const db = new DB(res);
+
+        db.insert(new Data(assign({}, { user_id: req.user._id }, req.body)));
+    }
+
+    public updateData(req: IRequest, res: express.Response) {
+        const db = new DB(res);
+
+        db.update_one(req.params.id, req.body, (data: Data) => {
+            res.status(200).json(data);
+        });
+    }
+
     public collections(req: IRequest, res: express.Response) {
         const db = new DB(res);
         db.findOne(
