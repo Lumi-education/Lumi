253596d6c31fbae3e5ce2d1287995d67f5f9a9f7
JPSchellenberg <jps@Lumi.education>
2018-01-18 00:32:30 +0100

    add grade_overview for user (closes #141)

--- src/client/pages/admin/users/user-page.tsx
+++ src/client/pages/admin/users/user-page.tsx
@@ -1,8 +1,9 @@
 // modules
 import * as React from 'react';
 import { connect } from 'react-redux';
+import { push } from 'lib/ui/actions';
 
-import { Paper } from 'material-ui';
+import { Paper, Tabs, Tab } from 'material-ui';
 
 // state
 import { IState } from 'client/state';
@@ -10,9 +11,11 @@ import { IState } from 'client/state';
 // modules
 import * as User from 'lib/users';
 import * as Groups from 'lib/groups';
+import * as Grades from 'lib/grades';
 
 interface IStateProps {
     user_id: string;
+    tab: string;
 }
 
 interface IDispatchProps {
@@ -36,13 +39,71 @@ export class AdminUserPage extends React.Component<IProps, {}> {
     public render() {
         return (
             <div>
-                <Paper>
-                    <User.UserContainer user_id={this.props.user_id}>
-                        <Groups.GroupsInputContainer
-                            user_id={this.props.user_id}
-                        />
-                    </User.UserContainer>
-                </Paper>
+                <Tabs
+                    style={{
+                        position: 'fixed',
+                        backgroundColor: '#FFFFFF',
+                        top: '64px',
+                        zIndex: 1099,
+                        width: '100%'
+                    }}
+                    tabItemContainerStyle={{
+                        background: 'linear-gradient(120deg, #8e44ad, #3498db)'
+                    }}
+                    value={this.props.tab}
+                >
+                    <Tab
+                        label="Settings"
+                        value="settings"
+                        onActive={() =>
+                            this.props.dispatch(
+                                push(
+                                    '/admin/users/' +
+                                        this.props.user_id +
+                                        '/settings'
+                                )
+                            )
+                        }
+                    />
+                    <Tab
+                        label="Grades"
+                        value="grades"
+                        onActive={() =>
+                            this.props.dispatch(
+                                push(
+                                    '/admin/users/' +
+                                        this.props.user_id +
+                                        '/grades'
+                                )
+                            )
+                        }
+                    />
+                </Tabs>
+                {(() => {
+                    switch (this.props.tab) {
+                        case 'settings':
+                        default:
+                            return (
+                                <Paper>
+                                    <User.UserContainer
+                                        user_id={this.props.user_id}
+                                    >
+                                        <Groups.GroupsInputContainer
+                                            user_id={this.props.user_id}
+                                        />
+                                    </User.UserContainer>
+                                </Paper>
+                            );
+                        case 'grades':
+                            return (
+                                <div>
+                                    <Grades.GradeListContainer
+                                        user_id={this.props.user_id}
+                                    />
+                                </div>
+                            );
+                    }
+                })()}
             </div>
         );
     }
@@ -50,7 +111,8 @@ export class AdminUserPage extends React.Component<IProps, {}> {
 
 function mapStateToProps(state: IState, ownProps): IStateProps {
     return {
-        user_id: ownProps.params.user_id
+        user_id: ownProps.params.user_id,
+        tab: ownProps.params.tab
     };
 }
 
