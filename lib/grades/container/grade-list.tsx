850af67fb0a82eeb8dc4aa3b24f5246750e5f976
JPSchellenberg <jps@Lumi.education>
2018-01-18 00:31:45 +0100

    add grade_list (closes #136)

---
+++ lib/grades/container/grade-list.tsx
@@ -0,0 +1,100 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+import * as debug from 'debug';
+
+import * as moment from 'moment';
+
+// types
+import { IState } from '../types';
+
+// components
+
+import { Avatar, Divider, List, ListItem } from 'material-ui';
+
+// modules
+import * as Grades from '../';
+import { get_grade_color } from 'lib/ui/utils';
+
+const log = debug('lumi:lib:grades:container:grade-list');
+
+interface IPassedProps {
+    user_id: string;
+}
+
+interface IStateProps extends IPassedProps {
+    grades: Grades.IGrade[];
+}
+
+interface IDispatchProps {
+    dispatch: (action) => void;
+}
+
+interface IProps extends IStateProps, IDispatchProps {}
+
+export class GradeListContainer extends React.Component<IProps, {}> {
+    constructor(props: IProps) {
+        super(props);
+    }
+
+    public componentWillMount() {
+        log('componentWillMount');
+
+        this.props.dispatch(Grades.actions.get_user_grades(this.props.user_id));
+    }
+
+    public render() {
+        return (
+            <List>
+                {this.props.grades.length === 0 ? (
+                    <ListItem key="nogrades" primaryText="No Grades" />
+                ) : null}
+                {this.props.grades.map(grade => (
+                    <div key={grade._id}>
+                        <ListItem
+                            leftAvatar={
+                                <Avatar
+                                    backgroundColor={get_grade_color(
+                                        grade.score * 100
+                                    )}
+                                    size={50}
+                                >
+                                    {grade.score * 100}
+                                </Avatar>
+                            }
+                            primaryText={grade.grade_type}
+                            secondaryText={
+                                <span>
+                                    <span>
+                                        {moment(grade.created_at).calendar()}
+                                    </span>
+                                    <br />
+                                    {grade.note}
+                                </span>
+                            }
+                            secondaryTextLines={2}
+                        />
+                        <Divider inset={true} />
+                    </div>
+                ))}
+            </List>
+        );
+    }
+}
+
+function mapStateToProps(state: IState, ownProps): IStateProps {
+    return {
+        grades: Grades.selectors.grades_for_user(state, ownProps.user_id),
+        user_id: ownProps.user_id
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        dispatch: action => dispatch(action)
+    };
+}
+
+export default connect<IStateProps, IDispatchProps, IPassedProps>(
+    mapStateToProps,
+    mapDispatchToProps
+)(GradeListContainer);
