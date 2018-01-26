1b43df96a459e8d32f04358df6de8275a48c8ea0
JPSchellenberg <jps@Lumi.education>
2018-01-26 14:17:23 +0100

    add sentry to server

--- src/server/boot.ts
+++ src/server/boot.ts
@@ -7,8 +7,8 @@ import wait_for_couchdb from './utils/wait_for_couchdb';
 import check_db from './db/check';
 import boot_socket from './core/socket';
 import webhook from './core/webhook';
-
 declare var process;
+import * as raven from 'raven';
 
 const debug = _debug('core');
 const express_debug = _debug('boot:express');
@@ -18,6 +18,19 @@ wait_for_couchdb(() => {
         boot();
     });
 });
+
+if (process.env.NODE_ENV === 'production') {
+    raven
+        .config(process.env.SENTRY, {
+            release: process.env.VERSION,
+            environment: process.env.NODE_ENV,
+            tags: {
+                branch: process.env.BRANCH,
+                component: 'server'
+            }
+        })
+        .install();
+}
 
 function boot() {
     debug('starting boot-sequence');
@@ -33,12 +46,7 @@ function boot() {
         });
     });
 
-    server.on('error', err => {
-        webhook({
-            username: 'System',
-            text: JSON.stringify(err)
-        });
-    });
+    server.on('error', raven.captureException);
 
     boot_socket(server);
 
