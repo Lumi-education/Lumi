f2b846c662da74250bb0a6b83ff9ba2daa8c67e9
JPSchellenberg <jps@Lumi.education>
2018-01-26 23:10:30 +0100

    disable action-error

--- src/client/store/middleware/debug.ts
+++ src/client/store/middleware/debug.ts
@@ -10,21 +10,21 @@ export default function debugMiddleware({ dispatch, getState }) {
     return next => action => {
         log(action.type, action);
 
-        try {
-            if (
-                action.type.substring(
-                    action.type.length - 5,
-                    action.type.length
-                ) === 'ERROR'
-            ) {
-                raven.captureMessage('ACTION', {
-                    level: 'error',
-                    extra: { action }
-                });
-            }
-        } catch (err) {
-            raven.captureException(err);
-        }
+        // try {
+        //     if (
+        //         action.type.substring(
+        //             action.type.length - 5,
+        //             action.type.length
+        //         ) === 'ERROR'
+        //     ) {
+        //         raven.captureMessage('ACTION', {
+        //             level: 'error',
+        //             extra: { action }
+        //         });
+        //     }
+        // } catch (err) {
+        //     raven.captureException(err);
+        // }
 
         raven.captureBreadcrumb({
             message: action.type,
