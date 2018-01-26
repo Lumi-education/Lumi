d0ac6835ba56925aa50b39753c106fc152b04fde
JPSchellenberg <jps@Lumi.education>
2018-01-26 15:02:07 +0100

    add raven to client-redux

--- src/client/boot.tsx
+++ src/client/boot.tsx
@@ -7,6 +7,20 @@ import { browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import * as injectTapEventPlugin from 'react-tap-event-plugin';
 
+import * as raven from 'raven-js';
+
+if (process.env.NODE_ENV === 'production') {
+    raven
+        .config(process.env.SENTRY, {
+            release: process.env.VERSION,
+            environment: process.env.NODE_ENV,
+            tags: {
+                component: 'client'
+            }
+        })
+        .install();
+}
+
 import Router from './router';
 import store from 'client/store';
 
--- src/client/store/middleware/debug.ts
+++ src/client/store/middleware/debug.ts
@@ -2,12 +2,36 @@ import { IState } from 'client/state';
 import { Dispatch } from 'redux';
 import * as debug from 'debug';
 import { assign } from 'lodash';
+import * as raven from 'raven-js';
 
 const log = debug('lumi:core:store:action:');
 
 export default function debugMiddleware({ dispatch, getState }) {
     return next => action => {
         log(action.type, action);
+
+        try {
+            if (
+                action.type.substring(
+                    action.type.length - 5,
+                    action.type.length
+                ) === 'ERROR'
+            ) {
+                raven.captureMessage('ACTION', {
+                    level: 'error',
+                    extra: { action }
+                });
+            }
+        } catch (err) {
+            raven.captureException(err);
+        }
+
+        raven.captureBreadcrumb({
+            message: action.type,
+            category: 'action',
+            data: action
+        });
+
         return next(action);
     };
 }
