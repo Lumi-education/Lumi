4125d6b9a82d2840a4006aa1913ef567b527d4b5
JPSchellenberg <jps@Lumi.education>
2018-02-02 20:55:18 +0100

    add raven to express

--- src/server/core/app.ts
+++ src/server/core/app.ts
@@ -3,15 +3,15 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as path from 'path';
 import * as compression from 'compression';
-
+import * as raven from 'raven';
 import routes from '../routes';
 
 const debug = _debug('server');
 
 const app: express.Application = express();
 
-// app.use(raven.requestHandler());
-// app.use(raven.errorHandler());
+app.use(raven.requestHandler());
+app.use(raven.errorHandler());
 
 app.use(bodyParser.json());
 app.use(
