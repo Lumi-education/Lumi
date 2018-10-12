c0423bf3c5fcec0f2b7d1f372a9767ee5808c6a5
JPSchellenberg <jps@Lumi.education>
2018-10-12 18:33:06 +0200

    add captive-page

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -1,13 +1,15 @@
 export { IState } from './types';
 import LoginContainer from './container/login';
 import PasswordContainer from './container/password';
+import CaptiveContainer from './container/captive';
 
 export { default as auth_reducer } from './reducer';
 import * as actions from './actions';
 
 const container = {
     Login: LoginContainer,
-    Password: PasswordContainer
+    Password: PasswordContainer,
+    Captive: CaptiveContainer
 };
 
 export { actions, container };
