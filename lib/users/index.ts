5281d58a96b55ba75c7a5faadfd5a14101e8531d
JPSchellenberg <jps@Lumi.education>
2018-10-29 03:28:22 +0100

    remove old code

--- lib/users/index.ts
+++ lib/users/index.ts
@@ -1,5 +1,4 @@
 export { IState, IUser, IUsersUI } from './types';
-export { default as UserContainer } from './container/user';
 export { default as users_reducer } from './reducer';
 import * as selectors from './selectors';
 import * as actions from './actions';
