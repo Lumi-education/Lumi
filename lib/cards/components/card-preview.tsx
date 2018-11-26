1a90aff9048d9dad4feda4a29a42cda7c6954002
JPSchellenberg <jps@Lumi.education>
2018-11-26 11:33:16 +0100

    add h5p-content-type-icons (closes #273)

--- lib/cards/components/card-preview.tsx
+++ lib/cards/components/card-preview.tsx
@@ -27,86 +27,78 @@ export default class CardPreviewComponent extends React.Component<IProps, {}> {
     public render() {
         const card = this.props.card;
         return (
-            <div id="card_preview" className="device-container ">
-                <div className="device-mockup iphone6_plus portrait black ">
-                    <div className="device ">
-                        <div className="screen ">
-                            <div
-                                style={{
-                                    height: '100%',
-                                    width: '100%',
-                                    overflow: 'scroll',
+            // <div id="card_preview" className="device-container ">
+            //     <div className="device-mockup iphone6_plus portrait black ">
+            //         <div className="device ">
+            //             <div className="screen ">
+            //                 <div
+            //                     style={{
+            //                         height: '100%',
+            //                         width: '100%',
+            //                         overflow: 'scroll',
 
-                                    background: UI.config.gradient_bg
-                                }}
-                            >
-                                {(() => {
-                                    switch (card.card_type) {
-                                        case 'multiplechoice':
-                                            return (
-                                                <MultiplechoiceComponent
-                                                    card_id={card._id || 'tmp'}
-                                                    text={card.text || ''}
-                                                    items={card.items || ['']}
-                                                    selected_items={[]}
-                                                    show_correct_values={true}
-                                                />
-                                            );
-                                        case 'text':
-                                            return (
-                                                <TextComponent
-                                                    card_id={card._id || 'tmp'}
-                                                    text={card.text || ''}
-                                                />
-                                            );
-                                        case 'freetext':
-                                            return (
-                                                <FreetextComponent
-                                                    card_id={card._id || 'tmp'}
-                                                    text={card.text || ''}
-                                                    answer={card.answer || ''}
-                                                />
-                                            );
-                                        case 'video':
-                                            return (
-                                                <VideoComponent
-                                                    card_id={card._id || 'tmp'}
-                                                    video_url={
-                                                        card.video_url || ''
-                                                    }
-                                                />
-                                            );
-                                        case 'upload':
-                                            return (
-                                                <UploadComponent
-                                                    card_id={card._id || 'tmp'}
-                                                    text={card.text || ''}
-                                                />
-                                            );
-                                        case 'h5p':
-                                            return (
-                                                <Paper>
-                                                    <H5PComponent
-                                                        key={card.content_id}
-                                                        content_id={
-                                                            card._id || ''
-                                                        }
-                                                        h5p_found={
-                                                            card.content_id
-                                                                ? true
-                                                                : false
-                                                        }
-                                                    />
-                                                </Paper>
-                                            );
-                                    }
-                                })()}
-                            </div>
-                        </div>
-                        <div className="button " />
-                    </div>
-                </div>
-            </div>
+            //                         background: UI.config.gradient_bg
+            //                     }}
+            //                 >
+            (() => {
+                switch (card.card_type) {
+                    case 'multiplechoice':
+                        return (
+                            <MultiplechoiceComponent
+                                card_id={card._id || 'tmp'}
+                                text={card.text || ''}
+                                items={card.items || ['']}
+                                selected_items={[]}
+                                show_correct_values={true}
+                            />
+                        );
+                    case 'text':
+                        return (
+                            <TextComponent
+                                card_id={card._id || 'tmp'}
+                                text={card.text || ''}
+                            />
+                        );
+                    case 'freetext':
+                        return (
+                            <FreetextComponent
+                                card_id={card._id || 'tmp'}
+                                text={card.text || ''}
+                                answer={card.answer || ''}
+                            />
+                        );
+                    case 'video':
+                        return (
+                            <VideoComponent
+                                card_id={card._id || 'tmp'}
+                                video_url={card.video_url || ''}
+                            />
+                        );
+                    case 'upload':
+                        return (
+                            <UploadComponent
+                                card_id={card._id || 'tmp'}
+                                text={card.text || ''}
+                            />
+                        );
+                    case 'h5p':
+                        return (
+                            <Paper>
+                                <H5PComponent
+                                    key={card.content_id}
+                                    content_id={card._id || ''}
+                                    h5p_found={card.content_id ? true : false}
+                                />
+                            </Paper>
+                        );
+                }
+            })()
+            //                 </div>
+            //             </div>
+            //             <div className="button " />
+            //         </div>
+            //     </div>
+            // </div>
         );
     }
 }
