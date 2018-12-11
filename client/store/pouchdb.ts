a735118b8ad0ed727306b4e2e88642b7b35593d2
JPSchellenberg <jps@Lumi.education>
2018-12-11 13:36:42 +0100

    add debug to pouchdb

--- client/store/pouchdb.ts
+++ client/store/pouchdb.ts
@@ -1,3 +1,4 @@
+import * as debug from 'debug';
 import PouchDB from 'pouchdb';
 import store from './';
 
@@ -5,9 +6,13 @@ import * as PouchDBFind from 'pouchdb-find';
 
 PouchDB.plugin(PouchDBFind);
 
-export const db_name = 'lumi';
+export const db_name = 'test';
+
+const log_info = debug('lumi:info:db');
+const log_error = debug('lumi:error:db');
 
 const db = new PouchDB(db_name);
+
 const remote_db = new PouchDB(`${window.location.origin}/api/v1/${db_name}`, {
     fetch: (url, opts) => {
         opts.headers.set('x-auth', window.localStorage.jwt_token);
@@ -16,27 +21,38 @@ const remote_db = new PouchDB(`${window.location.origin}/api/v1/${db_name}`, {
 });
 PouchDB.sync(db, remote_db, {
     live: true,
-    retry: true
+    retry: true,
+    filter: '_view',
+    view: `users/me`
 })
     .on('change', changes => {
+        log_info('changes', changes);
         if (changes.direction === 'pull') {
             store.dispatch({ type: 'DB_CHANGE', payload: changes.change.docs });
         }
     })
-    .on('paused', err => {
+    .on('paused', error => {
+        log_info('paused', error);
         // replication paused (e.g. replication up to date, user went offline)
     })
     .on('active', () => {
+        log_info('active');
         // replicate resumed (e.g. new changes replicating, user went back online)
     })
-    .on('denied', err => {
+    .on('denied', error => {
+        log_error('denied', error);
         // a document failed to replicate (e.g. due to permissions)
     })
     .on('complete', info => {
+        log_info('complete', info);
         //
     })
-    .on('error', err => {
+    .on('error', error => {
+        log_error('error', error);
         // handle error
     });
+
+declare var window;
+window.db = db;
 
 export default db;
