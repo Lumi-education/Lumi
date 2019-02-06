eab6e099eda4e60c285b4015d79cc9b764ce23ef
JPSchellenberg <jps@Lumi.education>
2019-02-06 12:36:22 +0100

    Client: Core: Remove Update-logic that belongs to pi-module

--- lib/core/reducer/status.ts
+++ lib/core/reducer/status.ts
@@ -29,43 +29,44 @@ const initialState: IStatus = {
 export default function(state: IStatus = initialState, action): IStatus {
     try {
         switch (action.type) {
-            case CORE_CHECK_UPDATE_REQUEST:
-                return assign({}, state, {
-                    update: assign({}, state.update, {
-                        request_status: 'pending'
-                    })
-                });
+            // case CORE_CHECK_UPDATE_REQUEST:
+            //     return assign({}, state, {
+            //         update: assign({}, state.update, {
+            //             request_status: 'pending'
+            //         })
+            //     });
 
-            case CORE_CHECK_UPDATE_ERROR:
-                return assign({}, state, {
-                    update: assign({}, state.update, {
-                        request_status: 'error'
-                    })
-                });
+            // case CORE_CHECK_UPDATE_ERROR:
+            //     return assign({}, state, {
+            //         update: assign({}, state.update, {
+            //             request_status: 'error'
+            //         })
+            //     });
 
-            case CORE_CHECK_UPDATE_SUCCESS:
-                return assign({}, state, {
-                    update: assign({}, action.payload, {
-                        request_status: 'success'
-                    })
-                });
+            // case CORE_CHECK_UPDATE_SUCCESS:
+            //     return assign({}, state, {
+            //         update: assign({}, action.payload, {
+            //             request_status: 'success'
+            //         })
+            //     });
 
-            case CORE_GET_ENV_SUCCESS:
-                return assign({}, state, { env: action.payload });
+            // case CORE_GET_ENV_SUCCESS:
+            //     return assign({}, state, { env: action.payload });
 
-            case CORE_UPDATE_ENV_SUCCESS:
-            case CORE_UPDATE_SYSTEM_SUCCESS:
-                return assign({}, state, {
-                    status_page: true,
-                    status_page_text:
-                        'System wird aktualisiert. Dies kann einige Minuten dauern. Bitte warten.'
-                });
-            case SYSTEM_SHUTDOWN:
-                return assign({}, state, {
-                    status_page: true,
-                    status_page_text:
-                        'System wird heruntergefahren. Bitte 20 Sekunden warten.'
-                });
+            // case CORE_UPDATE_ENV_SUCCESS:
+            // case CORE_UPDATE_SYSTEM_SUCCESS:
+            //     return assign({}, state, {
+            //         status_page: true,
+            //         status_page_text:
+            //             'System wird aktualisiert. Dies kann einige Minuten dauern. Bitte warten.'
+            //     });
+
+            // case SYSTEM_SHUTDOWN:
+            //     return assign({}, state, {
+            //         status_page: true,
+            //         status_page_text:
+            //             'System wird heruntergefahren. Bitte 20 Sekunden warten.'
+            //     });
 
             default:
                 return state;
